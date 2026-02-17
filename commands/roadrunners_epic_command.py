f98f25e04f94f14b3b0dc1960821b9d84131a010
Raphael Treccani-Chinelli <raphael.treccanichinelli@gmail.com>
2026-02-17 22:40:25 +0000

    wowow

--- commands/roadrunners_epic_command.py
+++ commands/roadrunners_epic_command.py
@@ -1,7 +1,7 @@
 import discord
 
 async def roadrunner(interaction: discord.Interaction):
-	await interaction.response.send_message(":road:")
+	await interaction.response.send_message("<:road:1459339557529980958>")
 
 
 module = {
